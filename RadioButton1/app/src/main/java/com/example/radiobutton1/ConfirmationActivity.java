561b71ea1fb46f346735d8e8d5436c4b8c3d7c48
yuchin1070 <a111221070@mail.shu.edu.tw>
2024-04-24 15:18:14 +0800

    0424

--- RadioButton1/app/src/main/java/com/example/radiobutton1/ConfirmationActivity.java
+++ RadioButton1/app/src/main/java/com/example/radiobutton1/ConfirmationActivity.java
@@ -16,20 +16,20 @@ public class ConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
 
-        // Retrieve data from Intent
+        // 從 Intent 中取得資料
         Intent intent = getIntent();
         String gender = intent.getStringExtra("gender");
         String ticketType = intent.getStringExtra("ticketType");
         int quantity = intent.getIntExtra("quantity", 0);
         int totalPrice = intent.getIntExtra("totalPrice", 0);
 
-        // Find TextViews in the layout
+        // 找到佈局中的 TextView
         TextView textViewGender = findViewById(R.id.textViewGender);
         TextView textViewTicketType = findViewById(R.id.textViewTicketType);
         TextView textViewQuantity = findViewById(R.id.textViewQuantity);
         TextView textViewTotalPrice = findViewById(R.id.textViewTotalPrice);
 
-        // Set data to TextViews
+        // 將資料設置到 TextView 上
         textViewGender.setText("性別: " + gender);
         textViewTicketType.setText("票種: " + ticketType);
         textViewQuantity.setText("購買張數: " + quantity);
--- RadioButton1/app/src/main/java/com/example/radiobutton1/MainActivity.java
+++ RadioButton1/app/src/main/java/com/example/radiobutton1/MainActivity.java
@@ -6,57 +6,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonConfirm = findViewById(R.id.buttonConfirm);
-        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get selected gender
-                RadioButton boy = findViewById(R.id.rdbBoy);
-                String gender = boy.isChecked() ? "男生" : "女生";
+                String outputStr = "";
 
-                // Get selected ticket type and price
-                RadioButton adult = findViewById(R.id.rdbAdult);
-                RadioButton child = findViewById(R.id.rdbChild);
-                RadioButton student = findViewById(R.id.rdbStudent);
-                String ticketType;
-                int ticketPrice;
-                if (adult.isChecked()) {
-                    ticketType = "成人";
+                RadioButton boy = (RadioButton) findViewById(R.id.rdbBoy);
+                RadioButton girl = (RadioButton) findViewById(R.id.rdbGirl);
+
+                if (boy.isChecked())
+                    outputStr += "性別: 男生\n";
+                else if (girl.isChecked())
+                    outputStr += "性別: 女生\n";
+
+                RadioGroup type = (RadioGroup) findViewById(R.id.rgType);
+
+                int selectedTypeId = type.getCheckedRadioButtonId();
+                double ticketPrice = 0;
+
+                if (selectedTypeId == R.id.rdbAdult) {
+                    outputStr += "票種: 全票\n";
                     ticketPrice = 500;
-                } else if (child.isChecked()) {
-                    ticketType = "孩童";
+                } else if (selectedTypeId == R.id.rdbChild) {
+                    outputStr += "票種: 兒童票\n";
                     ticketPrice = 250;
                 } else {
-                    ticketType = "學生";
+                    outputStr += "票種: 學生票\n";
                     ticketPrice = 400;
                 }
 
-                // Get quantity
-                TextView editTextQuantity = findViewById(R.id.editTextQuantity);
-                int quantity = Integer.parseInt(editTextQuantity.getText().toString());
+                EditText quantityEditText = findViewById(R.id.etQuantity);
+                int quantity = Integer.parseInt(quantityEditText.getText().toString());
+                double totalPrice = ticketPrice * quantity;
+                outputStr += "張數: " + quantity + "\n金額: $" + totalPrice;
 
-                // Calculate total price
-                int totalPrice = ticketPrice * quantity;
-
-                // Create intent for next activity
-                Intent intent = new Intent(MainActivity.this, ConfirmationActivity.class);
-                intent.putExtra("gender", gender);
-                intent.putExtra("ticketType", ticketType);
-                intent.putExtra("quantity", quantity);
-                intent.putExtra("totalPrice", totalPrice);
-                startActivity(intent);
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+                output.setText(outputStr);
             }
-        });
+        }));
     }
 }
